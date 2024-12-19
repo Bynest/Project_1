e9be563563001230618ddfcb6498070f47499938
ErnestA <ernestappleby@hotmail.co.uk>
2024-12-19 20:58:06 +0000

    2nd commit

--- Project_1-1/__init__.py
+++ Project_1-1/__init__.py
@@ -1,4 +1,13 @@
 # New Python
 
-initialV = 1+1
+initialV = 1 + 1
 print("The answer is", initialV)
+
+
+# install libraries
+# inputs
+# static parameters
+# wrangling
+# Exploratory
+# Initial automated methods
+# Validation of automated methods
