88177c7744660ec74c8551cf9c2604267d1c6ccf
ErnestA <ernestappleby@hotmail.co.uk>
2025-01-04 19:17:06 +0000

    Day 2, basic commands

--- Project_1-1/__init__.py
+++ Project_1-1/__init__.py
@@ -1,14 +1,32 @@
 # New Python
 
-initialV = 1 + 1
-print("The answer is", initialV)
-
-
 # install libraries
+import yfinance as yf
+import pandas as pd
+import numpy as np
+
+# get/set working directory
+import os
+
+os.getcwd()
+
+
+# Set up data
+dat = yf.Ticker("MSFT")
+tickerInfo = dir(dat)
+dat.info
+dat.calendar
+dat._price_history
+dat._shares
+dat.analyst_price_targets
+dat.quarterly_income_stmt
+dat.history(period="1mo")
+dat.option_chain(dat.options[0]).calls
+
+
 # inputs
 # static parameters
 # wrangling
 # Exploratory
 # Initial automated methods
 # Validation of automated methods
-
